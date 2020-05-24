d8af5bfef4c94632855ea8b6aea28e5bb3671e02
Kushagra Tripathi <51934770+Kayteee@users.noreply.github.com>
2020-05-24 23:32:42 +0530

    Add files via upload

---
+++ src/components/Form.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+
+
+const Form = props => (
+  <form onSubmit={props.getWeather}>
+      <input type="text" name="city" placeholder="City..." autoComplete="off"/>
+      <button>Get Weather</button>
+  </form>
+
+)
+
+
+
+
+
+
+export default Form;
